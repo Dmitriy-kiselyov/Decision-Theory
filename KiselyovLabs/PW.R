334f4ffa2dd3d8b0c68a1a4cb0274672d7c59722
Dmitriy-kiselyov <dmitriy.penetrator@gmail.com>
2017-10-14 11:47:27 +0300

    Speed up Potential Window

--- KiselyovLabs/PW.R
+++ KiselyovLabs/PW.R
@@ -6,15 +6,13 @@ source("kernelHelp.R")
 mc.PW.kernel = mc.kernel.Q #use this kernel
 
 #PW
-mc.PW = function(points, classes, u, h) {
-    dist = mc.getDistances(points, u, mc.euclideanDistance)
-    names(dist) = classes
-    dist = dist / h
-    dist = mc.PW.kernel(dist)
+mc.PW = function(distances, classes, u, h) {
+    distances = distances / h
+    distances = mc.PW.kernel(distances)
 
     sumByName = function(name, arr) sum(arr[names(arr) == name])
     classes = unique(classes)
-    wei = sapply(classes, sumByName, dist)
+    wei = sapply(classes, sumByName, distances)
     names(wei) = classes
 
     return(names(which.max(wei)))
@@ -28,10 +26,12 @@ mc.LOO.PW = function(points, classes, hLimits) {
     for (i in 1:n) {
         u = points[i,]
         teachSample = points[-i,]
+        distances = mc.getDistances(teachSample, u, mc.euclideanDistance)
+        names(distances) = classes[-i]
 
         for (j in 1:length(hLimits)) {
             h = hLimits[j]
-            bestClass = mc.PW(teachSample, classes[-i], u, h)
+            bestClass = mc.PW(distances, classes[-i], u, h)
             looY[j] = looY[j] + ifelse(bestClass == classes[i], 0, 1)
         }
     }
@@ -68,7 +68,9 @@ mc.draw.PW = function(points, classes, colors, h, xlim, ylim, step) {
             u = c(round(x, 1), round(y, 1)) #use round to aviod cases 0.1 + 0.2 = 0.3000000004
             if (any(apply(points, 1, function(v) all(v == u)))) next #do not classify known points
 
-            bestClass = mc.PW(points, classes, u, h)
+            distances = mc.getDistances(points, u, mc.euclideanDistance)
+            names(distances) = classes
+            bestClass = mc.PW(distances, classes, u, h)
 
             #draw new point
             points(u[1], u[2], col = colors[bestClass], pch = 21) #u
@@ -83,6 +85,6 @@ main = function() {
 
     #draw
     par(mfrow = c(1, 2))
-    h.opt = mc.draw.LOO.PW(petals, petalNames, hLimits = seq(0.5, 2, 0.5))
+    h.opt = mc.draw.LOO.PW(petals, petalNames, hLimits = seq(0.1, 1, 0.1))
     mc.draw.PW(petals, petalNames, colors = c("red", "green3", "blue"), h = h.opt, xlim = plot.limits(petals[, 1], 0.2), ylim = plot.limits(petals[, 2], 0.2), step = 0.1)
 }
